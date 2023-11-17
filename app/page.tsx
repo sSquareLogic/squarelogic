8ea69a029f2a58fc129269f40419ceb8cea012dd
VividTruthKeeper <mr.aydogdy02@gmail.com>
2023-11-17 16:46:42 +0900

    scroll smoother removed

--- app/page.tsx
+++ app/page.tsx
@@ -2,19 +2,16 @@ import About from "@/components/About";
 import Hero from "@/components/Hero";
 import Plans from "@/components/Plans";
 import Projects from "@/components/Projects";
-import ScrollSmooth from "@/hox/ScrollSmooth";
 import Selected from "@/components/Selected";
 
 export default function Home() {
   return (
-    <ScrollSmooth>
-      <main className="main py-[124px] flex flex-col gap-[100px]">
-        <Hero />
-        <Selected />
-        <About />
-        <Projects />
-        <Plans />
-      </main>
-    </ScrollSmooth>
+    <main className="main py-[124px] flex flex-col gap-[100px]">
+      <Hero />
+      <Selected />
+      <About />
+      <Projects />
+      <Plans />
+    </main>
   );
 }
