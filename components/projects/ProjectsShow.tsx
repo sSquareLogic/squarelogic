831c8051be787d2470ef6642aa41e2fa510f475f
VividTruthKeeper <mr.aydogdy02@gmail.com>
2023-11-30 21:48:59 +0900

    projects page responsive

--- components/projects/ProjectsShow.tsx
+++ components/projects/ProjectsShow.tsx
@@ -4,7 +4,7 @@ import ProjectImage from "./ProjectImage";
 const ProjectsShow = () => {
   return (
     <section className="projects-show">
-      <Container className="grid grid-cols-TWO gap-20">
+      <Container className="grid grid-cols-TWO gap-20 max-lg:gap-14 max-md:gap-10 max-[680px]:flex max-[680px]:flex-col max-[680px]:gap-6">
         <div className="projects-row flex flex-col gap-6">
           <ProjectImage
             description="The best tool for stop-motion animation"
@@ -22,7 +22,7 @@ const ProjectsShow = () => {
             title="Make Cents"
           />
         </div>
-        <div className="projects-row flex flex-col gap-6 mt-40">
+        <div className="projects-row flex flex-col gap-6 mt-40 max-md:mt-20 max-[680px]:mt-0">
           <ProjectImage
             description="The best tool for stop-motion animation"
             src="/projectshow1.png"
