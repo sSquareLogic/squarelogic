eed8f40ea13cf6a8eb3ff1cb72e5d61e97316bb0
VividTruthKeeper <mr.aydogdy02@gmail.com>
2023-12-03 04:16:19 +0900

    seo

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,12 +2,35 @@ import "./globals.css";
 
 import type { Metadata } from "next";
 import { Space_Grotesk } from "next/font/google";
+import colors from "@/settings/colors";
 
 const spacegr = Space_Grotesk({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
+  viewport: {
+    width: "device-width",
+    maximumScale: 1,
+    initialScale: 1,
+  },
+  applicationName: "squarelogic.space",
+  themeColor: colors.ACCENT,
+  colorScheme: "only light",
   title: "SquareLogic",
-  description: "SquareLogic",
+  description:
+    "SQUARELOGIC is a specialized creative studio focusing on strategic solutions, branding, design, and development, with a primary emphasis on serving the real estate and e-commerce industries.",
+  twitter: {
+    title: "SquareLogic",
+    description: "SQUARELOGIC is...",
+    site: "@site",
+    card: "summary_large_image",
+    images: ["/logo.png"],
+  },
+  openGraph: {
+    title: "SquareLogic",
+    description: "SQUARELOGIC is...",
+    siteName: "@site",
+    images: ["/logo.png"],
+  },
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
