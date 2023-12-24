d3e214354c0e73215be94b2cd97eb562822668fe
VividTruthKeeper <mr.aydogdy02@gmail.com>
2023-12-24 23:28:48 +0900

    workflow section animations removed

--- components/Home/workflow/Workflow.tsx
+++ components/Home/workflow/Workflow.tsx
@@ -12,6 +12,7 @@ import {
 } from "framer-motion";
 
 import colors from "@/settings/colors";
+import defaultTransition from "@/settings/transition";
 
 interface IWorkflowItem {
   title: string;
@@ -43,110 +44,50 @@ const items: IWorkflowItem[] = [
 ];
 
 interface IWorkflowItemProps {
-  container: RefObject<HTMLElement>;
-  order: number;
   item: IWorkflowItem;
-  setActiveItem: (order: number) => void;
 }
 
-const WorkflowItem = ({ container, order, item, setActiveItem }: IWorkflowItemProps) => {
-  const targetRef = useRef<HTMLDivElement>(null);
-  const start = -order * 70;
-  const isTargetInView = useInView(targetRef, {
-    root: container,
-  });
-  const controls = useAnimationControls();
-
-  const { scrollY } = useScroll();
-  const scrollVelocity = useVelocity(scrollY);
-  const smoothVelocity = useSpring(scrollVelocity, {
-    damping: 50,
-    stiffness: 400,
-  });
-  const velocityFactor = useTransform(smoothVelocity, [0, 1000], [0, 5], {
-    clamp: false,
-  });
-
-  useEffect(() => {
-    if (isTargetInView) {
-      if (velocityFactor.get() > 0) setActiveItem(order);
-      controls.start((i) =>
-        i === 0
-          ? { color: colors.WHITE }
-          : {
-              color: colors.WHITE,
-              height: "fit-content",
-            },
-      );
-    } else {
-      if (velocityFactor.get() < 0) setActiveItem(order - 1);
-      controls.start((i) =>
-        i === 0
-          ? { color: "#4D4D4D" }
-          : {
-              color: "#4D4D4D",
-              height: 0,
-            },
-      );
-    }
-  }, [isTargetInView, controls, setActiveItem, order, velocityFactor]);
-
+const WorkflowItem = ({ item }: IWorkflowItemProps) => {
   return (
     <>
-      <div className="absolute" style={{ top: `${start}vh` }} ref={targetRef}></div>
-      <motion.div
-        className="workflow-item flex flex-col gap-6"
-        initial={{ color: "#4D4D4D" }}
-        custom={0}
-        animate={controls}
-        transition={{ duration: 0.4, type: "tween" }}
-      >
-        <h6 className="text-[64px] font-bold max-md:text-[56px] max-sm:text-[48px]">{item.title}</h6>
+      <div className="workflow-item flex flex-col gap-6">
+        <motion.h6
+          className="text-[64px] font-bold max-md:text-[56px] max-sm:text-[48px]"
+          initial={{ opacity: "20%" }}
+          whileInView={{ opacity: "100%" }}
+        >
+          {item.title}
+        </motion.h6>
         <motion.p
           className="text-[32px] leading-[140%] overflow-hidden max-md:text-2xl max-sm:text-lg"
-          initial={{ height: 0 }}
-          custom={1}
-          animate={controls}
+          initial={{ opacity: "20%" }}
+          whileInView={{ opacity: "100%" }}
+          transition={defaultTransition}
         >
           {item.description}
         </motion.p>
-      </motion.div>
+      </div>
     </>
   );
 };
 
 const Workflow = () => {
-  const height: number = (items.length + 0.7) * 100;
-  const containerRef = useRef<HTMLElement>(null);
-  const itemRef = useRef<HTMLDivElement>(null);
-  const [activeItem, setActiveItem] = useState<number>(0);
-
   return (
-    <section className="workflow" style={{ height: `${height}vh` }} ref={containerRef}>
+    <section className="workflow">
       <div className="custom-container max-w-[1384px] px-8 mx-auto w-full h-full max-lg:px-5 max-md:px-4">
-        <motion.div
-          className="sticky top-[100px]"
-          animate={{
-            top: 100 - (activeItem * 200 * items.length) / 7,
-          }}
-          transition={{
-            duration: 0.4,
-            type: "tween",
-            ease: "easeOut",
-          }}
-        >
+        <div className="sticky top-[100px]">
           <div className="relative grid grid-cols-ABOUT gap-20 max-md:flex max-md:flex-col max-md:gap-8">
-            <div className="flex flex-col gap-6 max-md:gap-4">
+            <div className="flex flex-col h-fit sticky top-6 left-0 gap-6 max-md:gap-4">
               <span className="text-lg font-medium">(Our workflow)</span>
               <h6 className="text-[40px] font-bold max-md:text-[32px]">Explore our workflow</h6>
             </div>
-            <div ref={itemRef} className="flex flex-col gap-10">
+            <div className="flex flex-col gap-10">
               {items.map((item, i) => (
-                <WorkflowItem key={i} item={item} container={containerRef} order={i} setActiveItem={setActiveItem} />
+                <WorkflowItem key={i} item={item} />
               ))}
             </div>
           </div>
-        </motion.div>
+        </div>
       </div>
     </section>
   );
