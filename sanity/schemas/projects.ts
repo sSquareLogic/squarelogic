3ccc5cdd5ebcd2157bfdc9af637fd1a9ef3f80df
VividTruthKeeper <mr.aydogdy02@gmail.com>
2023-12-29 02:15:31 +0900

    projects schema descriptions added

--- sanity/schemas/projects.ts
+++ sanity/schemas/projects.ts
@@ -42,16 +42,19 @@ const projects = {
   fields: [
     {
       name: "hero",
+
       type: "object",
       title: "Hero",
       fields: [
         {
           name: "title",
+          description: "The title of the page, the first thing people will see when they visit.",
           type: "string",
           title: "Title",
         },
         {
           name: "description",
+          description: "This will be under the title of the page.",
           type: "array",
           title: "Description",
           of: [
@@ -69,11 +72,13 @@ const projects = {
       fields: [
         {
           name: "projects",
+          description: "Add projects here. *ONLY 2 ROWS ARE SUPPORTED*",
           type: "object",
           title: "Projects",
           fields: [
             {
               name: "row_1",
+              description: "This is row 1.",
               type: "array",
               title: "Row 1",
               of: [
@@ -111,6 +116,7 @@ const projects = {
             },
             {
               name: "row_2",
+              description: "This is row 2.",
               type: "array",
               title: "Row 2",
               of: [
@@ -152,16 +158,19 @@ const projects = {
     },
     {
       name: "metadata",
+      description: "Used for SEO and social media.",
       type: "object",
       title: "Metadata",
       fields: [
         {
           name: "title",
+          description: "This is the title of the page, the one that is written on the tab of the browser.",
           type: "string",
           title: "Title",
         },
         {
           name: "description",
+          description: "This is the description of the page, the one that is shown on search engines.",
           type: "string",
           title: "Description",
         },
