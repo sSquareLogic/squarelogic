5613688440fc40cd0fc675d4fc0df4441d8217d8
VividTruthKeeper <mr.aydogdy02@gmail.com>
2023-12-30 16:37:35 +0900

    faq items cursor loop

--- components/Home/faq/FAQList.tsx
+++ components/Home/faq/FAQList.tsx
@@ -1,3 +1,4 @@
+import CursorHover from "@/components/animated/CursorHover";
 import FAQItem from "./FAQItem";
 import { IFAQ } from "@/sanity/schemas/home";
 
@@ -9,7 +10,9 @@ const FAQList = ({ items }: IProps) => {
   return (
     <ul className="flex flex-col gap-6">
       {items.map((faq, i) => (
-        <FAQItem key={i} {...faq} position={i} />
+        <CursorHover key={i} state="loop">
+          <FAQItem {...faq} position={i} />
+        </CursorHover>
       ))}
     </ul>
   );
