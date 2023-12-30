b841cc3fd9853691ba83b6852d004206ca043100
VividTruthKeeper <mr.aydogdy02@gmail.com>
2023-12-30 16:02:26 +0900

    features responsive

--- components/Home/features/Feature.tsx
+++ components/Home/features/Feature.tsx
@@ -11,18 +11,24 @@ interface IProps {
 const Feature = ({ name, description, icon, accent }: IProps) => {
   return (
     <div
-      className="feature p-10 flex flex-col gap-8 bg-LIGHTBLACK rounded-3xl"
+      className="feature p-10 flex flex-col gap-8 bg-LIGHTBLACK rounded-3xl max-sm:gap-6 max-sm:p-6"
       style={accent ? { background: colors.ACCENT } : {}}
     >
       <div
         className="rounded-full bg-ACCENT flex items-center justify-center p-4 w-fit h-fit"
         style={accent ? { background: colors.BLACK } : {}}
       >
-        <Image src={icon} alt={name} width={48} height={48} className="object-contain w-12 h-12" />
+        <Image
+          src={icon}
+          alt={name}
+          width={48}
+          height={48}
+          className="object-contain w-12 h-12 max-md:w-9 max-md:h-9 max-sm:w-6 max-sm:h-6"
+        />
       </div>
-      <div className="flex flex-col gap-6" style={accent ? { color: colors.BLACK } : {}}>
-        <h6 className="text-2xl font-bold">{name}</h6>
-        <p className="text-lg leading-[140%]">{description}</p>
+      <div className="flex flex-col gap-6 max-sm:gap-4" style={accent ? { color: colors.BLACK } : {}}>
+        <h6 className="text-2xl font-bold max-lg:text-xl">{name}</h6>
+        <p className="text-lg leading-[140%] max-lg:text-base">{description}</p>
       </div>
     </div>
   );
--- components/Home/features/Features.tsx
+++ components/Home/features/Features.tsx
@@ -24,7 +24,7 @@ const Features = () => {
   ];
   return (
     <section className="features">
-      <Container className="grid grid-cols-THIRTEE gap-10">
+      <Container className="grid grid-cols-THIRTEE gap-10 max-lg:flex max-lg:flex-col max-lg:gap-12">
         <div className="flex flex-col gap-6 max-md:gap-4">
           <span className="text-lg font-medium">(Our features)</span>
           <h6 className="text-[40px] font-bold max-md:text-[32px]">Why to choose us and not others?</h6>
