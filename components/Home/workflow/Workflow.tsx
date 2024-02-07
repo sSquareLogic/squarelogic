2c655a1a671ab17a744df1e606ee559898779173
VividTruthKeeper <mr.aydogdy02@gmail.com>
2024-02-07 15:38:42 +0900

    workflow section changed

--- components/Home/workflow/Workflow.tsx
+++ components/Home/workflow/Workflow.tsx
@@ -1,3 +1,4 @@
+import Container from "@/components/Container";
 import CursorHover from "@/components/animated/CursorHover";
 import { IWorkflow } from "@/sanity/schemas/home";
 import Title from "@/components/Title";
@@ -6,21 +7,19 @@ import WorkflowItem from "./WorkflowItem";
 const Workflow = ({ items, subtitle }: IWorkflow) => {
   return (
     <section className="workflow">
-      <div className="custom-container max-w-[1384px] px-8 mx-auto w-full h-full max-lg:px-5 max-md:px-4">
-        <div className="relative grid grid-cols-THIRTEE gap-10 max-lg:flex max-lg:flex-col max-lg:gap-8">
-          <div className="flex flex-col h-fit sticky top-6 left-0 gap-6 max-md:gap-4 max-lg:static">
-            <span className="text-lg font-medium">(Our workflow)</span>
-            <Title text={subtitle} />
-          </div>
-          <div className="flex flex-col gap-10">
-            {items.map((item, i) => (
-              <CursorHover key={i} state="loop">
-                <WorkflowItem {...item} />
-              </CursorHover>
-            ))}
-          </div>
+      <Container className="flex flex-col gap-12 max-lg:gap-8">
+        <div className="flex flex-col gap-6 max-md:gap-4">
+          <span className="text-lg font-medium">(Our workflow)</span>
+          <Title text={subtitle} />
         </div>
-      </div>
+        <div className="flex flex-col border-b border-BLACK3">
+          {items.map((item, i) => (
+            <CursorHover key={i} state="loop">
+              <WorkflowItem {...item} />
+            </CursorHover>
+          ))}
+        </div>
+      </Container>
     </section>
   );
 };
--- components/Home/workflow/WorkflowItem.tsx
+++ components/Home/workflow/WorkflowItem.tsx
@@ -8,19 +8,19 @@ import { motion } from "framer-motion";
 const WorkflowItem = ({ description, title }: IWorkflow["items"][any]) => {
   return (
     <>
-      <div className="workflow-item flex flex-col gap-6">
+      <div className="workflow-item grid grid-cols-THIRTEE items-center py-10 gap-10 border-t border-BLACK3 max-md:flex max-md:flex-col max-md:items-start">
         <motion.h6
           transition={defaultTransition}
-          className="text-[64px] font-bold max-md:text-[56px] max-sm:text-[48px]"
-          initial={{ opacity: "20%" }}
-          whileInView={{ opacity: "100%" }}
+          className="text-2xl font-bold font-INTER -tracking-[1%]"
+          initial={{ opacity: "20%", x: "-5%" }}
+          whileInView={{ opacity: "100%", x: "0" }}
         >
           {title}
         </motion.h6>
         <motion.div
-          className="text-[32px] leading-[140%] overflow-hidden max-md:text-2xl max-sm:text-lg"
-          initial={{ opacity: "20%" }}
-          whileInView={{ opacity: "100%" }}
+          className="text-lg leading-[140%] overflow-hidden"
+          initial={{ opacity: "20%", x: "-5%" }}
+          whileInView={{ opacity: "100%", x: "0" }}
           transition={defaultTransition}
         >
           <PortableText value={description} />
