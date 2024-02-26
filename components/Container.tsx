4cd7bffa938b8f10ce0ce28dbd55c598de5d1cdd
VividTruthKeeper <mr.aydogdy02@gmail.com>
2024-02-26 22:52:39 +0900

    Refactor Container component styles

--- components/Container.tsx
+++ components/Container.tsx
@@ -6,7 +6,7 @@ interface IProps extends PropsWithChildren {
 
 const Container = ({ children, className }: IProps) => {
   return (
-    <div className="max-w-[1384px] px-8 mx-auto w-full transition-all max-lg:px-5 overflow-hidden max-md:px-4">
+    <div className="max-w-[1384px] px-8 mx-auto w-full overflow-hidden transition-all max-lg:px-5 max-md:px-4">
       {className ? <div className={className}>{children}</div> : children}
     </div>
   );
