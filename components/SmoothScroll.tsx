6110947e6abce9442e8e90762166b5d70ca440ba
VividTruthKeeper <mr.aydogdy02@gmail.com>
2024-03-12 16:28:25 +0900

    smooth scroll fixed

--- components/SmoothScroll.tsx
+++ components/SmoothScroll.tsx
@@ -1,3 +1,4 @@
+//@ts-nocheck
 "use client";
 
 import { PropsWithChildren } from "react";
@@ -5,7 +6,20 @@ import { ReactLenis } from "@studio-freight/react-lenis";
 
 function SmoothScroll({ children }: PropsWithChildren) {
   return (
-    <ReactLenis root options={{ lerp: 0.1, duration: 1.5, smoothTouch: true }}>
+    <ReactLenis
+      root
+      options={{
+        lerp: 0.1,
+        duration: 3,
+        easing: (t) => Math.min(1, 1.001 - Math.pow(4, -5 * t)),
+        direction: "vertical",
+        gestureDirection: "vertical",
+        smooth: true,
+        smoothTouch: false,
+        touchMultiplier: 2,
+        infinite: false,
+      }}
+    >
       {children}
     </ReactLenis>
   );
