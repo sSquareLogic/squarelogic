c339894826e4245494b543242f0f1a54fd1298f1
VividTruthKeeper <mr.aydogdy02@gmail.com>
2024-03-12 14:37:15 +0900

    Refactor ContactBtn component

--- components/Home/contact/ContactBtn.tsx
+++ components/Home/contact/ContactBtn.tsx
@@ -1,28 +1,15 @@
-"use client";
-
-import CursorHover from "@/components/animated/CursorHover";
 import Image from "next/image";
-import { useMediaQuery } from "usehooks-ts";
 
 const ContactBtn = () => {
-  const breakpoints = {
-    md: useMediaQuery("(min-width: 768px)", {
-      defaultValue: false,
-    }),
-  };
-
   return (
-    <CursorHover fullWidth={!breakpoints.md}>
-      <a
-        href={"https://calendly.com/mikejames1311005/squarelogic"}
-        target="_blank"
-        style={{ width: !breakpoints.md ? "100%" : "fit-content" }}
-        className="flex gap-4 items-center justify-center min-w-52 rounded-[56px] bg-ACCENT text-center py-6 px-6 text-BLACK font-bold text-xl font-INTER -tracking-[1%]"
-      >
-        <span>Book A Call</span>
-        <Image src={"/arrow-big.svg"} alt="Arrow" width={20} height={20} />
-      </a>
-    </CursorHover>
+    <a
+      href={"https://calendly.com/mikejames1311005/squarelogic"}
+      target="_blank"
+      className="flex gap-4 items-center justify-center min-w-52 w-fit rounded-[56px] bg-ACCENT text-center py-6 px-6 text-BLACK font-bold text-xl font-INTER -tracking-[1%] max-md:w-full"
+    >
+      <span>Book A Call</span>
+      <Image src={"/arrow-big.svg"} alt="Arrow" width={20} height={20} />
+    </a>
   );
 };
 export default ContactBtn;
