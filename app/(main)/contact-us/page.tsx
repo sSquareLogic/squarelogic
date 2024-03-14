8edd9124c0d96504a310fe0a7139e735fa494c83
VividTruthKeeper <mr.aydogdy02@gmail.com>
2024-03-14 12:09:23 +0900

    Update hero title in contact page

--- app/(main)/contact-us/page.tsx
+++ app/(main)/contact-us/page.tsx
@@ -18,7 +18,7 @@ const Contact = async () => {
   return (
     <main className="contact-us py-[124px] flex flex-col gap-[200px] max-md:py-20 max-md:gap-[150px] max-sm:gap-[100px] max-sm:py-10 max-[450px]:py-6">
       <h1 hidden>{contactData[0].metadata.description}</h1>
-      <Hero title={contactData[0].metadata.title} description={contactData[0].metadata.description} />
+      <Hero title={"SQUARELOGIC"} description={contactData[0].metadata.description} />
       <ContactUs />
     </main>
   );
